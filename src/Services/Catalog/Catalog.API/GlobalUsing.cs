2e14801248c1afdfb7552952fe8ea09e10ee0d09
scarlett-lim <appleladada@gmail.com>
2025-01-16 00:15:18 +0800

    Data seeding using marten library

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -8,4 +8,6 @@ global using Catalog.API.Exceptions;
 global using FluentValidation;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.Exceptions.Handler;
+global using Catalog.API.Data;
+
 
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,4 @@
 #region Before building web app
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +6,6 @@ var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR(config =>  //tell the program mediatr where to get the handler
 {
     config.RegisterServicesFromAssembly(assembly);
-
     //add validation behavior as a pipeline behavior into mediatr
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
@@ -20,6 +18,11 @@ builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.InitializeMartenWith<CatalogInitialData>();  //initialize the data for development environment
+}
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
